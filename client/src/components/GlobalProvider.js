414fe45c8c6478cdda1c560672d6e3eb71ba3707
Your Name <davidnielsen215@gmail.com>
2019-06-18 14:52:44 -0600

    updated database tuesday afternoon

--- client/src/components/GlobalProvider.js
+++ client/src/components/GlobalProvider.js
@@ -39,7 +39,7 @@ constructor (props) {
         
     
 }
-}
+
 
 export default GlobalProvider
 
---
+++ routes/workoutRouter.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const workoutRouter = express.Router()
+
+
+const Workout = require('../models/workoutSchema')
+
+
+
+workoutRouter.get('/', (req, res, next) => {
+    Workout.find((err, workouts) => {
+        if(err){
+            res.status(500)
+            return res.send(err)
+        }
+        return res.status(200).send(workouts)
+        
+    })
+})
+
+workoutRouter.post('/new', (req, res, next) => {
+    const newWorkout = new Workout(req.body) 
+    newWorkout.save((err, newSavedWorkout) => {
+        if(err){
+            res.status(500)
+            return res.send(err)
+        }
+        return res.status(201).send(newSavedWorkout)
+    })
+})
+
+module.exports = workoutRouter
+
+
+
+
+
+// const express = require('express')
+// const workoutRouter = express.Router()
+// const Workout = require('../models/workoutSchema.js/index.js')
+
+// workoutRouter.route('/')
+
+//     .get((request, response) => {
+//         Workout.find((err, workouts)=> {
+//             if(err){
+//                 return response.status(500).send(err)
+//             }
+//             return response.status(200).send(workouts)
+//         })
+//     })
+
+//     .post((request, response) => {
+//         const newWorkout = new Workout(request.body)
+//         newWorkout.save((err, new_workout) =>{
+//             if(err) {
+//                 return response.status(500).send(err)
+//             }
+//             return response.status(201).send(new_workout)
+//         })
+//     })
+
+//     workoutRouter.route('/:id')
+
+//     .get((request, response) =>{
+//         Workout.findById(request.params.id, (err, workout) => {
+//             if(err) {
+//                 return response.status(500).send(err)
+//             }
+//             return response.status(200).send(workout)
+//         })
+//     })
+
+//     .delete((request, response) => {
+//         Workout.findByIdAndDelete(request.params.id, (err, workout) => {
+//             if(err) {
+//                 return response.status(500).send(err)
+//             }
+//             return response.status(200).send('Deleted Workout')
+//         })
+//     })
+
+//     .put((request, response) => {
+//         Workout.findByIdAndUpdate(request.params.id, request.body, {new: true}, 
+//             (err, workout) => {
+//             if(err) {
+//                 return response.status(500).send(err)
+//             }
+//             return response.status(201).send(workout)
+//         })
+//     })
+
+// module.exports = workoutRouter
